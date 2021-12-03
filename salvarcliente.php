270b8c789301e68fbb934cdd9fe7b3b53845207d
Gustavo Sampaio <37808461+gstsdev@users.noreply.github.com>
2021-12-03 10:43:46 -0300

    Restructuring table display layout

--- salvarcliente.php
+++ salvarcliente.php
@@ -9,30 +9,34 @@ $emailCliente = $_GET["email"];
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Salvar Cliente</title>
 </head>
+
 <body>
     <table border="1">
-        <tr>
-            <td>Nome Completo</td>
-            <td><?php echo $nomeCliente; ?></td>
-        </tr>
-        <tr>
-            <td>CPF</td>
-            <td><?php echo $cpfCliente; ?></td>
-        </tr>
-        <tr>
-            <td>Telefone</td>
-            <td><?php echo $telefoneCliente; ?></td>
-        </tr>
-        <tr>
-            <td>E-mail</td>
-            <td><?php echo $emailCliente; ?></td>
-        </tr>
+        <thead>
+            <tr>
+                <th>Nome Completo</th>
+                <th>CPF</th>
+                <th>Telefone</th>
+                <th>E-mail</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+
+                <td><?php echo $nomeCliente; ?></td>
+                <td><?php echo $cpfCliente; ?></td>
+                <td><?php echo $telefoneCliente; ?></td>
+                <td><?php echo $emailCliente; ?></td>
+            </tr>
+        </tbody>
     </table>
 </body>
+
 </html>
