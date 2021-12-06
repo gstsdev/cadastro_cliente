9433a390d3884685e2c9da01cc8923c9144b27fa
Gustavo Sampaio <37808461+gstsdev@users.noreply.github.com>
2021-12-06 09:21:59 -0300

    Initial implementation of logout

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,25 +8,28 @@
     <title>Cliente</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-    
 
-<h1>Cadastro de Clientes</h1>
-<p>Sistema para cadastro de clientes.</p>
 
-<form action="salvarcliente.php" method="get">
-    <label for="nome">Nome do cliente</label>
-    <input type="text" name="nome" id="nome"><br>
-    <label for="cpf">CPF</label>
-    <input type="text" name="cpf" id="cpf"><br>
-    <label for="fone">telefone</label>
-    <input type="phone" name="fone" id="fone"><br>
-    <label for="email">email</label>
-    <input type="email" name="email" id="email"><br>
+    <h1>Cadastro de Clientes</h1>
+    <p>Sistema para cadastro de clientes. <a href="logout.php">Sair</a></p>
 
-    <input type="submit" name="salvar" value="Salvar Cliente"/>
-</form>
+    <form action="salvarcliente.php" method="get">
+        <label for="nome">Nome do cliente</label>
+        <input type="text" name="nome" id="nome"><br>
+        <label for="cpf">CPF</label>
+        <input type="text" name="cpf" id="cpf"><br>
+        <label for="fone">telefone</label>
+        <input type="phone" name="fone" id="fone"><br>
+        <label for="email">email</label>
+        <input type="email" name="email" id="email"><br>
+
+        <input type="submit" name="salvar" value="Salvar Cliente" />
+    </form>
+
 
 
 </body>
+
 </html>
